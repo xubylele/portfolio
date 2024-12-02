043bb75597e27144b865d8638b57a6de0df6aa9d
Xuby <diazxavier27@gmail.com>
2024-12-02 13:28:31 -0300

    Add GitHub Actions workflow for deployment to GitHub Pages and configure site settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://dev.xuby.cl',
+  base: '/'
 });
